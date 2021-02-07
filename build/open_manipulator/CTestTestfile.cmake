778000010533764a93789eedad33cb6fdda956fc
ZhengQiushi <woaiwo6666@qq.com>
2021-02-07 19:37:14 +0800

    修正了.git未删除的问题

--- build/open_manipulator/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sp/sp__ws/sp_armor/src/open_manipulator
-# Build directory: /home/sp/sp__ws/sp_armor/build/open_manipulator
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/open_manipulator_simulations/open_manipulator_gazebo/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;std_msgs;gazebo_ros;gazebo_ros_control;controller_manager;xacro;urdf".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "open_manipulator_gazebo"
-PROJECT_SPACE_DIR = "/home/sp/sp__ws/sp_armor/devel"
-PROJECT_VERSION = "1.1.0"
